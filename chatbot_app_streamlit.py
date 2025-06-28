9f345b939d6d284c7023b581b824b909bd0930fc
KIRAN P <langotikiran1@gmail.com>
2025-06-28 21:24:57 +0530

    Update chatbot_app_streamlit.py

--- chatbot_app_streamlit.py
+++ chatbot_app_streamlit.py
@@ -1,3 +1,15 @@
+import nltk
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
+
 import streamlit as st
 import json
 import string
